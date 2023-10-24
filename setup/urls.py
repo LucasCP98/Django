7a841171d51a7bdc946fe2eb33cfaf46acd1552c
LucasCP98 <lucascp1998@gmail.com>
2023-10-24 19:49:18 -0300

    urls.py adicionado em usuarios

--- setup/urls.py
+++ setup/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('galeria.urls')),
+                  path('', include('usuarios.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # a linha a cima + Static(..., serve para referenciar o media, static etc que estão no arquivo
 # settings do setup.
---
+++ usuarios/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from usuarios.views import *
+
+# urlpatterns = [
+#     path('', index, name='index'),
+#     path('imagem/<int:foto_id>', imagem, name='imagem'),
+#     path('buscar', buscar, name='buscar')
+# ]
